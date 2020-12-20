45185dbe577cea62874d4a16e2d034e4513fe14d
AaronNusrala <aaronnusrala@gmail.com>
2020-12-20 14:17:22 -0500

    initial commit

---
+++ BinaryString/BinaryStringConverter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace BinaryString
+{
+	public class BinaryStringConverter
+	{
+		/// <summary>
+		/// Given a string of 1 and 0, convert the binary number to its integer value.
+		/// Assume that the first digit in the string is the least significant bit.
+		/// Throw an argument exception if the binaryString parameter is not a valid binary string
+		/// </summary>
+		/// <param name="binaryString"></param>
+		/// <returns></returns>
+		public int ConvertString(string binaryString)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
---
+++ BinaryStringTests/BinaryStringConverterTests.cs
@@ -0,0 +1,45 @@
+using System;
+using BinaryString;
+using NUnit.Framework;
+
+namespace BinaryStringTests
+{
+	public class Tests
+	{
+		private BinaryStringConverter _converter;
+
+		[SetUp]
+		public void Setup() => _converter = new BinaryStringConverter();
+
+		[TestCase("0", 0)]
+		[TestCase("1", 1)]
+		[TestCase("00", 0)]
+		[TestCase("10", 1)]
+		[TestCase("01", 2)]
+		[TestCase("11", 3)]
+		[TestCase("000", 0)]
+		[TestCase("100", 1)]
+		[TestCase("010", 2)]
+		[TestCase("110", 3)]
+		[TestCase("001", 4)]
+		[TestCase("101", 5)]
+		[TestCase("011", 6)]
+		[TestCase("111", 7)]
+		[TestCase("01010100", 42)]
+		public void Converts_String_To_Int(string input, int expectedResult)
+		{
+			var result = _converter.ConvertString(input);
+			Assert.AreEqual(expectedResult, result);
+		}
+
+		[TestCase("INVALID")]
+		[TestCase("")]
+		[TestCase(null)]
+		[TestCase(" ")]
+		[TestCase("0102011")]
+		public void Throw_Exception_For_Invalid_Input(string input)
+		{
+			Assert.Throws<ArgumentException>(() => _converter.ConvertString(input));
+		}
+	}
+}
